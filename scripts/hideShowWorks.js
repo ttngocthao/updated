b1ab60ef1b277b145e0a2f65b881fe56df0f7e1f
Thao Thi Ngoc Truong <ttngocthao_87@yahoo.com>
2019-01-12 23:12:44 +0000

    commit

--- scripts/hideShowWorks.js
+++ scripts/hideShowWorks.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.moreBtn').click(function(){
         $(this).siblings('.timeline-event-text').slideToggle();
-
-        $(this).children('i').toggleClass('clickedBtn')
+        $(this).children('i').toggleClass('clickedBtn');
+        var textBtn=$(this).children('.text');
+        textBtn.text(textBtn.text() == 'Show less' ? 'Click for details':'Show less');
     })
     
 
